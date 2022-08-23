4c48fffaf3af8121a590c1e6ceb8878762b89499
Marius Cosmin Ciuca <ciucacosmin109@gmail.com>
2022-08-23 20:10:39 +0300

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
     * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
+*/
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
